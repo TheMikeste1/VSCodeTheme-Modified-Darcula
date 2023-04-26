9313aff1ff01e30610e57837358735c3acc9de33
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2023-04-25 19:48:13 -0600

    Added additional definitions

--- example.cpp
+++ example.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 
+using MyType = int;
+
 namespace t {
   constexpr int TEST = 1;
   const int TEST2 = 2;
 }
 
 /**
- * @brief TEST <code>Markup<</code>
+ * @brief TEST <code>Markup</code>
  */
 class TestClass {
+
+    int tester = 0;
+
     void thing()
     {
-        int a = 1;
+        MyType a = 1;
         int b = 2;
         int c = a + b;
         this->thing();
+        this->tester = 5;
     }
 
-    template <typename T>
+    template <typename T, size_t SizeT>
     void thing2()
     {
-        T a = 1;
+        T a = SizeT;
         T b = 2;
         T c = a + b;
         this->thing();
@@ -30,6 +36,10 @@ class TestClass {
 using namespace t;
 // Comment
 int main() {
+    constexpr int TESTER = 1;
+    const int TESTER2 = 1;
+
+
     std::cout << "Hello, World!\n" << TEST << TEST2 << std::endl;
     TEST = 2;
     TEST2 = 3;
